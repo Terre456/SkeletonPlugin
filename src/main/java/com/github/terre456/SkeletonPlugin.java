dd22bc280eed8a7e9e8cc4da04ff33518b478dbb
Boris <sabredeboris@gmail.com>
2026-05-30 19:35:02 +0200

    complete ig

--- src/main/java/com/github/terre456/SkeletonPlugin.java
+++ src/main/java/com/github/terre456/SkeletonPlugin.java
@@ -6,13 +6,6 @@ public class SkeletonPlugin extends JavaPlugin{
   @Override
   public void onEnable(){
     getServer().getPluginManager().registerEvents(new SkeletonBehaviorListener(), this);
-    getLogger().info("on enable called");
-    System.out.println("starting PLugin for skeleton");
   }
 
-  @Override
-  public void onDisable(){
-    getLogger().info("on disable called");
-    System.out.println("stop plugin for Skeleton");
-  }
 }
